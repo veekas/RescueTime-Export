e77ffd7b368821717de4d986b65165d16f0cdd47
veekas <veekas@veekasmeansprogress.com>
2017-06-29 02:24:23 -0700

    changed to append, TODOs added for auto-update

--- RescueTimeExport.py
+++ RescueTimeExport.py
@@ -7,25 +7,28 @@
 # forked from Chris Burgess (xurizaemon)
 
 import urllib
+import datetime
 
 apiKey = "API_KEY"
 fileDirectory = ""
-filePrefix = "RescueTimeData"
+filePrefix = "RescueTime Data"
 
 def main():
     print "RescueTime Data Exporter"
     print "Dates in format YYYY-MM-DD"
-    date_s = raw_input("Start Date: ")
-    date_e = raw_input("End Date: ")
+    date_s = raw_input("Start Date: ") #TODO auto-populate the date of last modified
+    date_e = raw_input("End Date: ") #TODO change to 'yesterday's' date: yesterday = datetime.now() - timedelta(days=1); yesterday.strftime('%Y%m%d')
     print "Getting Data for Interval", date_s, "to", date_e
     params = urllib.urlencode({'key':apiKey, 'perspective':'interval', 'format':'csv', 'restrict_begin':date_s, 'restrict_end':date_e})
     u = urllib.urlopen("https://www.rescuetime.com/anapi/data", params)
     CSVdata = u.read()
-    filePath = fileDirectory + filePrefix + date_s.replace("-", "") + "-" + date_e.replace("-", "") + ".csv"
-    f = open(filePath, "w")
+    filePath = fileDirectory + filePrefix + ".csv" #simplified and generalized the filepath
+    f = open(filePath, "a") #changed to append the existing csv file instead of creating a new one
+    #TODO delete the duplicate header row that is created with each update
     f.write(CSVdata)
     f.close()
     print "Data Saved to", filePath
     print ""
+    #TODO make this process repeat daily/weekly/etc
 
 main()
